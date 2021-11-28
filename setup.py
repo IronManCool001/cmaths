2017448789cab3f41479b83eee51788ec10fb022
Debaditya Malakar <63513685+IronManCool001@users.noreply.github.com>
2021-11-28 09:14:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cmaths',
     version='0.1.0',    
     description='A python package to deal with maths',
-    url='#',
+    url='https://github.com/IronManCool001/cmaths',
     author='Debaditya Malakar',
     author_email='technicalfriend.help@gmail.com',
     license='MIT',
