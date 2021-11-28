49c5335b1a479c60a9767b814a657b89433fb6c0
Debaditya Malakar <63513685+IronManCool001@users.noreply.github.com>
2021-11-28 09:12:48 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cmaths',
+    version='0.1.0',    
+    description='A python package to deal with maths',
+    url='#',
+    author='Debaditya Malakar',
+    author_email='technicalfriend.help@gmail.com',
+    license='MIT',
+    packages=['cmaths'],
+    install_requires=["wheel"],
+)
